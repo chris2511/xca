efdff83226b7b6c37ab80724cde888286ead542a
chris2511 <chris2511>
2003-03-16 21:46:21 +0000

    put CRLs in database

---
+++ lib/db_crl.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2001 Christian Hohnstaedt.
+ *
+ *  All rights reserved.
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without 
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the author nor the names of its contributors may be 
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * This program links to software with different licenses from:
+ *
+ *	http://www.openssl.org which includes cryptographic software
+ * 	written by Eric Young (eay@cryptsoft.com)"
+ *
+ *	http://www.sleepycat.com
+ *
+ *	http://www.trolltech.com
+ * 
+ *
+ *
+ * http://www.hohnstaedt.de/xca
+ * email: christian@hohnstaedt.de
+ *
+ * $Id$
+ *
+ */                           
+
+
+#include "db_crl.h"
+
+
+db_crl::db_crl(DbEnv *dbe, string DBfile, QListView *l, DbTxn *tid)
+	:db_base(dbe, DBfile, "crldb", tid)
+{
+	listView = l;
+	loadContainer();
+	crlicon = loadImg("crl.png");
+	listView->addColumn(tr("Issuer"));
+	listView->addColumn(tr("Count"));
+	updateView();
+}
+
+pki_base *db_crl::newPKI(){
+	return new pki_crl();
+}
+
+
+
+void db_crl::updateViewPKI(pki_base *pki)
+{
+        db_base::updateViewPKI(pki);
+        if (! pki) return;
+        QListViewItem *current = (QListViewItem *)pki->getPointer();
+        if (!current) return;
+	current->setPixmap(0, *crlicon);
+	QString typec[]={tr("Empty"), tr("CA"), tr("Client"), tr("Server")};
+	current->setText(1, ((pki_crl *)pki)->issuerName().c_str());
+	current->setText(2, QString::number(((pki_crl *)pki)->numRev()));
+	
+}
