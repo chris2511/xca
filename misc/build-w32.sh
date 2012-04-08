8b25d7f0863b6b911f06a43dbf4a5c51df393e35
Christian Hohnstaedt <chohnstaedt@innominate.com>
2012-04-08 10:45:04 +0200

    Update to Openssl 1.0.1 on W32 platform

--- misc/build-w32.sh
+++ misc/build-w32.sh
@@ -8,7 +8,7 @@ LIBTOOL_DIR="libtool-2.2.6b"
 LIBTOOL_GZ="${LIBTOOL_DIR}".tar.gz
 LIBTOOL_DL="http://ftp.gnu.org/gnu/libtool/${LIBTOOL_GZ}"
 
-OPENSSL_DIR="openssl-1.0.0e"
+OPENSSL_DIR="openssl-1.0.1"
 OPENSSL_GZ="${OPENSSL_DIR}".tar.gz
 OPENSSL_DL="http://openssl.org/source/${OPENSSL_GZ}"
 OPENSSL_PATCH="$XCA_DIR/misc/openssl-1.0.0-mingw32-cross.patch"
