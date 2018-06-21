chris2511/xca

commit dcd940b6b89900c9365e1d88055b4adf01f3f80c

SF Feat. Req. #57 More options for Distinguished Name
Make explicit DN subject entries configurable
per database.

widgets/NewX509.cpp
@@ -26,16 +26,6 @@
 #include "lib/oid.h"
 #include "lib/func.h"
 
-int NewX509::name_nid[] = {
-	NID_countryName,
-	NID_stateOrProvinceName,
-	NID_localityName,
-	NID_organizationName,
-	NID_organizationalUnitName,
-	NID_commonName,
-	NID_pkcs9_emailAddress
-};
-
 NewX509::NewX509(QWidget *parent)
 	:QDialog(parent)
 {
@@ -44,6 +34,8 @@ NewX509::NewX509(QWidget *parent)
 	dn_nid = *MainWindow::dn_nid;
 	aia_nid = *MainWindow::aia_nid;
 	attr_nid << NID_pkcs9_unstructuredName << NID_pkcs9_challengePassword;
+	foreach(QString dn, MainWindow::explicit_dn.split(","))
+		expl_dn_nid << OBJ_sn2nid(CCHAR(dn));
 
 	QStringList keys;
 
@@ -131,19 +123,57 @@ NewX509::NewX509(QWidget *parent)
 	X509V3_set_ctx(&ext_ctx, NULL , NULL, NULL, NULL, 0);
 	X509V3_set_ctx_nodb(&ext_ctx);
 
-	QList<QLabel *> nameLabel;
-	nameLabel << LcountryName << LstateOrProvinceName << LlocalityName <<
-	LorganisationName << LorganisationalUnitName << LcommonName <<
-	LemailAddress;
+	// Setup dnWidget
+	QMap<int, QString> tooltips;
+	tooltips[NID_countryName] = tr("Country code");
+	tooltips[NID_stateOrProvinceName] = tr("State or Province");
+	tooltips[NID_localityName] = tr("Locality");
+	tooltips[NID_organizationName] = tr("Organisation");
+	tooltips[NID_organizationalUnitName] = tr("Organisational unit");
+	tooltips[NID_commonName] = tr("Common name");
+	tooltips[NID_pkcs9_emailAddress] = tr("E-Mail address");
 
-	for(int i=0; i<nameLabel.count(); i++) {
-		nameLabel[i]->setText(OBJ_nid2ln(name_nid[i]));
-		QString tt = nameLabel[i]->toolTip();
-		nameLabel[i]->setToolTip(QString("[%1] %2").
-			arg(OBJ_nid2sn(name_nid[i])).arg(tt));
-		name_ptr[i] = (QLineEdit *)nameLabel[i]->buddy();
-		setupLineEditByNid(name_nid[i], name_ptr[i]);
+	if (dnWidget->layout())
+		delete dnWidget->layout();
+	QGridLayout *dnLayout = new QGridLayout(dnWidget);
+	dnLayout->setAlignment(Qt::AlignTop);
+	dnLayout->setSpacing(6);
+	dnLayout->setMargin(0);
+	int n = 1, col = 0;
+
+	description = new QLineEdit(this);
+	description->setToolTip(tr("This name is only used internally and does not appear in the resulting certificate"));
+	QLabel *label = new QLabel(this);
+	label->setText(tr("Internal name"));
+	dnLayout->addWidget(label, 0, 0);
+	dnLayout->addWidget(description, 0, 1);
+
+	QWidget::setTabOrder(description, extDNlist);
+	QLineEdit *old = description;
+	foreach(int nid, expl_dn_nid) {
+		QLabel *label;
+		QLineEdit *edit;
+
+		label = new QLabel(this);
+		label->setText(OBJ_nid2ln(nid));
+		label->setToolTip(QString("[%1] %2").
+			arg(OBJ_nid2sn(nid)).arg(tooltips[nid]));
+
+		edit = new QLineEdit(this);
+		setupLineEditByNid(nid, edit);
+		nameEdits << nameEdit(nid, edit, label);
+
+		dnLayout->addWidget(label, n, col);
+		dnLayout->addWidget(edit, n, col +1);
+		n++;
+		if (n > expl_dn_nid.size()/2 && col == 0) {
+			col = 2;
+			n = expl_dn_nid.size() & 1 ? 0 : 1;
+		}
+		QWidget::setTabOrder(old, edit);
+		old = edit;
 	}
+
 	// Setup Request Attributes
 	if (attrWidget->layout())
 		delete attrWidget->layout();
@@ -232,13 +262,13 @@ void NewX509::addReqAttributes(pki_x509req *req)
 
 void NewX509::setTemp(pki_temp *temp)
 {
-	QString text = tr("Create");
+	QString text = tr("Create XCA template");
 	if (temp->getIntName() != "--") {
 		description->setText(temp->getIntName());
 		description->setDisabled(true);
-		text = tr("Edit");
+		text = tr("Edit XCA template");
 	}
-	capt->setText(text + " " + tr("XCA template"));
+	capt->setText(text);
 	tabWidget->removeTab(0);
 	privKeyBox->setEnabled(false);
 	validityBox->setEnabled(false);
@@ -501,9 +531,9 @@ void NewX509::on_genKeyBut_clicked()
 {
 	QString name = description->text();
 	if (name.isEmpty())
-		name = commonName->text();
+		name = dnEntryByNid(NID_commonName);
 	if (name.isEmpty())
-		name = emailAddress->text();
+		name = dnEntryByNid(NID_pkcs9_emailAddress);
 	emit genKey(name);
 }
 
@@ -633,9 +663,8 @@ x509name NewX509::getX509name(int _throw)
 	int j, row, nid;
 
 	try {
-		for (j = 0; j<EXPLICIT_NAME_CNT; j++) {
-			nid = name_nid[j];
-			x.addEntryByNid(nid, name_ptr[j]->text());
+		foreach(nameEdit ne, nameEdits) {
+			x.addEntryByNid(ne.nid, ne.edit->text());
 		}
 		row = extDNlist->rowCount();
 		for (j=0; j<row; j++) {
@@ -654,24 +683,38 @@ x509name NewX509::getX509name(int _throw)
 	return x;
 }
 
+QString NewX509::dnEntryByNid(int nid)
+{
+	foreach(nameEdit ne, nameEdits) {
+		if (ne.nid == nid && !ne.edit->text().isEmpty())
+			return ne.edit->text();
+	}
+	for (int j=0; j<extDNlist->rowCount(); j++) {
+		QStringList l = extDNlist->getRow(j);
+	        if (OBJ_txt2nid(CCHAR(l[0])) == nid && !l[1].isEmpty())
+			return l[1];
+	}
+	return QString();
+}
+
 void NewX509::setX509name(const x509name &n)
 {
-	int i,j;
-
 	extDNlist->deleteAllRows();
-	for (j=0; j<EXPLICIT_NAME_CNT; j++) {
-		name_ptr[j]->setText("");
+	foreach(nameEdit ne, nameEdits) {
+		ne.edit->setText("");
 	}
-	for (i=0, j=0; i< n.entryCount(); i++) {
+	for (int i=0; i< n.entryCount(); i++) {
 		int nid = n.nid(i);
+		bool done = false;
 		QStringList sl = n.entryList(i);
-		for ( ; j<EXPLICIT_NAME_CNT; j++) {
-			if (nid == name_nid[j] && name_ptr[j]->text().isEmpty()) {
-				name_ptr[j]->setText(sl[2]);
+		foreach(nameEdit ne, nameEdits) {
+			if (nid == ne.nid && ne.edit->text().isEmpty()) {
+				ne.edit->setText(sl[2]);
+				done = true;
 				break;
 			}
 		}
-		if (j == EXPLICIT_NAME_CNT) {
+		if (!done) {
 			extDNlist->addRow(sl.mid(1, 2));
 		}
 	}
@@ -999,7 +1042,8 @@ void NewX509::accept()
 		}
 	}
 	if (description->text().isEmpty() && !fromReqCB->isChecked()) {
-		if (commonName->text().isEmpty()) {
+		QString cn = dnEntryByNid(NID_commonName);
+		if (cn.isEmpty()) {
 			gotoTab(1);
 			xcaWarning msg(this,
 				tr("The internal name and the common name are empty.\nPlease set at least the internal name."));
@@ -1010,7 +1054,7 @@ void NewX509::accept()
 			}
 			return;
 		} else {
-			description->setText(commonName->text());
+			description->setText(cn);
 		}
 	}
 	if (keyList->count() == 0 && keyList->isEnabled() &&

lib/pki_temp.cpp
@@ -131,15 +131,14 @@ static int bitsToInt(extList &el, int nid, bool *crit)
 
 extList pki_temp::fromCert(pki_x509super *cert_or_req)
 {
-	int i;
 	x509name n;
 	extList el = cert_or_req->getV3ext();
 
 	nsComment = "";
 
 	n = cert_or_req->getSubject();
-	for (i=0; i<EXPLICIT_NAME_CNT; i++) {
-		int nid = NewX509::name_nid[i];
+	foreach(QString sn, MainWindow::explicit_dn) {
+		int nid =OBJ_sn2nid(CCHAR(sn));
 		QString ne = n.popEntryByNid(nid);
 		if (!ne.isNull())
 			xname.addEntryByNid(nid, ne);

ui/NewX509.ui
@@ -7,7 +7,7 @@
     <x>0</x>
     <y>0</y>
     <width>712</width>
-    <height>613</height>
+    <height>634</height>
    </rect>
   </property>
   <layout class="QVBoxLayout" name="verticalLayout_2">
@@ -116,8 +116,7 @@
               <number>6</number>
              </property>
              <item row="0" column="1">
-              <widget class="QComboBox" name="reqList">
-              </widget>
+              <widget class="QComboBox" name="reqList"/>
              </item>
              <item row="1" column="1">
               <widget class="QPushButton" name="showReqBut">
@@ -390,13 +389,7 @@
       <attribute name="title">
        <string>Subject</string>
       </attribute>
-      <layout class="QVBoxLayout">
-       <property name="spacing">
-        <number>6</number>
-       </property>
-       <property name="margin">
-        <number>9</number>
-       </property>
+      <layout class="QVBoxLayout" name="verticalLayout_7">
        <item>
         <widget class="QGroupBox" name="distNameBox">
          <property name="toolTip">
@@ -405,136 +398,9 @@
          <property name="title">
           <string notr="true">Distinguished name</string>
          </property>
-         <layout class="QVBoxLayout" name="_2">
+         <layout class="QVBoxLayout" name="verticalLayout_6">
           <item>
-           <layout class="QGridLayout" name="_3">
-            <item row="3" column="3">
-             <widget class="QLineEdit" name="emailAddress">
-              <property name="maxLength">
-               <number>60</number>
-              </property>
-             </widget>
-            </item>
-            <item row="0" column="2">
-             <widget class="QLabel" name="LorganisationName">
-              <property name="toolTip">
-               <string>Organisation</string>
-              </property>
-              <property name="buddy">
-               <cstring>organisationName</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="0" column="1">
-             <widget class="QLineEdit" name="description">
-              <property name="toolTip">
-               <string>This name is only used internally and does not appear in the resulting certificate</string>
-              </property>
-              <property name="maxLength">
-               <number>100</number>
-              </property>
-             </widget>
-            </item>
-            <item row="1" column="1">
-             <widget class="QLineEdit" name="countryName">
-              <property name="toolTip">
-               <string>Must be exactly 2 letter of size (DE, UK)</string>
-              </property>
-              <property name="maxLength">
-               <number>2</number>
-              </property>
-             </widget>
-            </item>
-            <item row="2" column="3">
-             <widget class="QLineEdit" name="commonName">
-              <property name="maxLength">
-               <number>64</number>
-              </property>
-             </widget>
-            </item>
-            <item row="1" column="0">
-             <widget class="QLabel" name="LcountryName">
-              <property name="toolTip">
-               <string>Country code</string>
-              </property>
-              <property name="buddy">
-               <cstring>countryName</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="2" column="0">
-             <widget class="QLabel" name="LstateOrProvinceName">
-              <property name="toolTip">
-               <string>State or Province</string>
-              </property>
-              <property name="buddy">
-               <cstring>stateOrProvinceName</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="2" column="1">
-             <widget class="QLineEdit" name="stateOrProvinceName"/>
-            </item>
-            <item row="3" column="0">
-             <widget class="QLabel" name="LlocalityName">
-              <property name="toolTip">
-               <string>Locality</string>
-              </property>
-              <property name="buddy">
-               <cstring>localityName</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="1" column="2">
-             <widget class="QLabel" name="LorganisationalUnitName">
-              <property name="toolTip">
-               <string>Organisational unit</string>
-              </property>
-              <property name="buddy">
-               <cstring>organisationalUnitName</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="3" column="2">
-             <widget class="QLabel" name="LemailAddress">
-              <property name="toolTip">
-               <string>E-Mail address</string>
-              </property>
-              <property name="buddy">
-               <cstring>emailAddress</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="3" column="1">
-             <widget class="QLineEdit" name="localityName"/>
-            </item>
-            <item row="1" column="3">
-             <widget class="QLineEdit" name="organisationalUnitName"/>
-            </item>
-            <item row="0" column="0">
-             <widget class="QLabel" name="TextLabel1_3_2_2">
-              <property name="text">
-               <string>Internal name</string>
-              </property>
-              <property name="buddy">
-               <cstring>description</cstring>
-              </property>
-             </widget>
-            </item>
-            <item row="0" column="3">
-             <widget class="QLineEdit" name="organisationName"/>
-            </item>
-            <item row="2" column="2">
-             <widget class="QLabel" name="LcommonName">
-              <property name="toolTip">
-               <string>Common name</string>
-              </property>
-              <property name="buddy">
-               <cstring>commonName</cstring>
-              </property>
-             </widget>
-            </item>
-           </layout>
+           <widget class="QWidget" name="dnWidget" native="true"/>
           </item>
           <item>
            <widget class="Line" name="Line1_2">
@@ -1562,37 +1428,38 @@
   </customwidget>
  </customwidgets>
  <tabstops>
-  <tabstop>tabWidget</tabstop>
   <tabstop>fromReqCB</tabstop>
   <tabstop>reqList</tabstop>
   <tabstop>copyReqExtCB</tabstop>
   <tabstop>showReqBut</tabstop>
+  <tabstop>reqSubChange</tabstop>
   <tabstop>selfSignRB</tabstop>
   <tabstop>serialNr</tabstop>
   <tabstop>foreignSignRB</tabstop>
   <tabstop>certList</tabstop>
+  <tabstop>hashAlgo</tabstop>
   <tabstop>tempList</tabstop>
+  <tabstop>applyExtensions</tabstop>
+  <tabstop>applySubject</tabstop>
   <tabstop>applyTemplate</tabstop>
-  <tabstop>description</tabstop>
-  <tabstop>countryName</tabstop>
-  <tabstop>stateOrProvinceName</tabstop>
-  <tabstop>localityName</tabstop>
-  <tabstop>organisationName</tabstop>
-  <tabstop>organisationalUnitName</tabstop>
-  <tabstop>commonName</tabstop>
-  <tabstop>emailAddress</tabstop>
+  <tabstop>extDNlist</tabstop>
   <tabstop>extDNadd</tabstop>
   <tabstop>extDNdel</tabstop>
   <tabstop>keyList</tabstop>
+  <tabstop>usedKeysToo</tabstop>
   <tabstop>genKeyBut</tabstop>
   <tabstop>basicCA</tabstop>
   <tabstop>basicPath</tabstop>
   <tabstop>bcCritical</tabstop>
   <tabstop>subKey</tabstop>
   <tabstop>authKey</tabstop>
+  <tabstop>notBefore</tabstop>
+  <tabstop>notAfter</tabstop>
   <tabstop>validNumber</tabstop>
-  <tabstop>validRange</tabstop>
   <tabstop>midnightCB</tabstop>
+  <tabstop>checkBox</tabstop>
+  <tabstop>noWellDefinedExpDate</tabstop>
+  <tabstop>validRange</tabstop>
   <tabstop>applyTime</tabstop>
   <tabstop>subAltName</tabstop>
   <tabstop>editSubAlt</tabstop>
@@ -1615,6 +1482,10 @@
   <tabstop>nsCaPolicyUrl</tabstop>
   <tabstop>nsSslServerName</tabstop>
   <tabstop>nsComment</tabstop>
+  <tabstop>nconf_data</tabstop>
+  <tabstop>adv_validate</tabstop>
+  <tabstop>buttonBox</tabstop>
+  <tabstop>tabWidget</tabstop>
  </tabstops>
  <resources/>
  <connections>

ui/Options.ui
@@ -6,8 +6,8 @@
    <rect>
     <x>0</x>
     <y>0</y>
-    <width>494</width>
-    <height>529</height>
+    <width>635</width>
+    <height>500</height>
    </rect>
   </property>
   <layout class="QVBoxLayout" name="verticalLayout_3">
@@ -33,175 +33,274 @@
     </widget>
    </item>
    <item>
-    <widget class="QGroupBox" name="groupBox">
-     <property name="title">
-      <string>Mandatory subject entries</string>
+    <widget class="QTabWidget" name="tabWidget">
+     <property name="currentIndex">
+      <number>1</number>
      </property>
-     <layout class="QHBoxLayout" name="horizontalLayout">
-      <item>
-       <layout class="QVBoxLayout">
-        <property name="spacing">
-         <number>6</number>
-        </property>
-        <property name="margin">
-         <number>0</number>
-        </property>
-        <item>
-         <widget class="QComboBox" name="extDNobj"/>
-        </item>
-        <item>
-         <widget class="QListWidget" name="extDNlist"/>
-        </item>
-       </layout>
-      </item>
-      <item>
-       <layout class="QVBoxLayout" name="verticalLayout_2">
-        <item>
-         <widget class="QPushButton" name="extDNadd">
-          <property name="text">
-           <string>Add</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="extDNdel">
-          <property name="text">
-           <string>Delete</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <spacer>
-          <property name="orientation">
-           <enum>Qt::Vertical</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>20</width>
-            <height>40</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-       </layout>
-      </item>
-     </layout>
-    </widget>
-   </item>
-   <item>
-    <layout class="QHBoxLayout">
-     <property name="spacing">
-      <number>6</number>
-     </property>
-     <property name="margin">
-      <number>0</number>
-     </property>
-     <item>
-      <widget class="QLabel" name="label_3">
-       <property name="text">
-        <string>Default hash algorithm</string>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="hashBox" name="hashAlgo"/>
-     </item>
-    </layout>
-   </item>
-   <item>
-    <layout class="QHBoxLayout">
-     <property name="spacing">
-      <number>6</number>
-     </property>
-     <property name="margin">
-      <number>0</number>
-     </property>
-     <item>
-      <widget class="QLabel" name="label">
-       <property name="text">
-        <string>String types</string>
-       </property>
-      </widget>
-     </item>
-     <item>
-      <widget class="QComboBox" name="mbstring"/>
-     </item>
-    </layout>
-   </item>
-   <item>
-    <widget class="QCheckBox" name="suppress">
-     <property name="text">
-      <string>Suppress success messages</string>
-     </property>
-    </widget>
-   </item>
-   <item>
-    <widget class="QCheckBox" name="noColorize">
-     <property name="text">
-      <string>Don't colorize expired certificates</string>
-     </property>
-    </widget>
-   </item>
-   <item>
-    <widget class="QGroupBox" name="groupBox_2">
-     <property name="title">
-      <string>PKCS#11 provider</string>
-     </property>
-     <layout class="QHBoxLayout" name="horizontalLayout_2">
-      <item>
-       <widget class="QListWidget" name="pkcs11List"/>
-      </item>
-      <item>
-       <layout class="QVBoxLayout" name="verticalLayout">
-        <item>
-         <widget class="QPushButton" name="addButton">
-          <property name="sizePolicy">
-           <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-            <horstretch>0</horstretch>
-            <verstretch>0</verstretch>
-           </sizepolicy>
-          </property>
-          <property name="text">
-           <string>Add</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="removeButton">
-          <property name="sizePolicy">
-           <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
-            <horstretch>0</horstretch>
-            <verstretch>0</verstretch>
-           </sizepolicy>
-          </property>
-          <property name="text">
-           <string>Remove</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <widget class="QPushButton" name="searchPkcs11">
-          <property name="text">
-           <string>Search</string>
-          </property>
-         </widget>
-        </item>
-        <item>
-         <spacer name="verticalSpacer">
-          <property name="orientation">
-           <enum>Qt::Vertical</enum>
-          </property>
-          <property name="sizeHint" stdset="0">
-           <size>
-            <width>77</width>
-            <height>17</height>
-           </size>
-          </property>
-         </spacer>
-        </item>
-       </layout>
-      </item>
-     </layout>
+     <widget class="QWidget" name="tab">
+      <attribute name="title">
+       <string>Settings</string>
+      </attribute>
+      <layout class="QVBoxLayout" name="verticalLayout_6">
+       <item>
+        <layout class="QHBoxLayout">
+         <property name="spacing">
+          <number>6</number>
+         </property>
+         <property name="margin">
+          <number>0</number>
+         </property>
+         <item>
+          <widget class="QLabel" name="label_3">
+           <property name="text">
+            <string>Default hash algorithm</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <widget class="hashBox" name="hashAlgo"/>
+         </item>
+        </layout>
+       </item>
+       <item>
+        <layout class="QHBoxLayout">
+         <property name="spacing">
+          <number>6</number>
+         </property>
+         <property name="margin">
+          <number>0</number>
+         </property>
+         <item>
+          <widget class="QLabel" name="label">
+           <property name="text">
+            <string>String types</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <widget class="QComboBox" name="mbstring"/>
+         </item>
+        </layout>
+       </item>
+       <item>
+        <widget class="QCheckBox" name="suppress">
+         <property name="text">
+          <string>Suppress success messages</string>
+         </property>
+        </widget>
+       </item>
+       <item>
+        <widget class="QCheckBox" name="noColorize">
+         <property name="text">
+          <string>Don't colorize expired certificates</string>
+         </property>
+        </widget>
+       </item>
+       <item>
+        <spacer name="verticalSpacer_2">
+         <property name="orientation">
+          <enum>Qt::Vertical</enum>
+         </property>
+         <property name="sizeHint" stdset="0">
+          <size>
+           <width>20</width>
+           <height>241</height>
+          </size>
+         </property>
+        </spacer>
+       </item>
+      </layout>
+     </widget>
+     <widget class="QWidget" name="tab_2">
+      <attribute name="title">
+       <string>Distinguished name</string>
+      </attribute>
+      <layout class="QVBoxLayout" name="verticalLayout_5">
+       <item>
+        <widget class="QGroupBox" name="groupBox">
+         <property name="title">
+          <string>Mandatory subject entries</string>
+         </property>
+         <layout class="QHBoxLayout" name="horizontalLayout">
+          <item>
+           <layout class="QVBoxLayout">
+            <property name="spacing">
+             <number>6</number>
+            </property>
+            <property name="margin">
+             <number>0</number>
+            </property>
+            <item>
+             <widget class="QComboBox" name="extDNobj"/>
+            </item>
+            <item>
+             <widget class="QListWidget" name="extDNlist"/>
+            </item>
+           </layout>
+          </item>
+          <item>
+           <layout class="QVBoxLayout" name="verticalLayout_2">
+            <item>
+             <widget class="QPushButton" name="extDNadd">
+              <property name="text">
+               <string>Add</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QPushButton" name="extDNdel">
+              <property name="text">
+               <string>Delete</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <spacer>
+              <property name="orientation">
+               <enum>Qt::Vertical</enum>
+              </property>
+              <property name="sizeHint" stdset="0">
+               <size>
+                <width>20</width>
+                <height>40</height>
+               </size>
+              </property>
+             </spacer>
+            </item>
+           </layout>
+          </item>
+         </layout>
+        </widget>
+       </item>
+       <item>
+        <widget class="QGroupBox" name="groupBox_2">
+         <property name="title">
+          <string>Explicit subject entries</string>
+         </property>
+         <layout class="QHBoxLayout" name="horizontalLayout_3">
+          <item>
+           <layout class="QVBoxLayout" name="_2">
+            <property name="spacing">
+             <number>6</number>
+            </property>
+            <property name="margin">
+             <number>0</number>
+            </property>
+            <item>
+             <widget class="QComboBox" name="expDNobj"/>
+            </item>
+            <item>
+             <widget class="QListWidget" name="expDNlist">
+              <property name="dragDropMode">
+               <enum>QAbstractItemView::InternalMove</enum>
+              </property>
+             </widget>
+            </item>
+           </layout>
+          </item>
+          <item>
+           <layout class="QVBoxLayout" name="verticalLayout_4">
+            <item>
+             <widget class="QPushButton" name="expDNadd">
+              <property name="text">
+               <string>Add</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QPushButton" name="expDNdel">
+              <property name="text">
+               <string>Delete</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <widget class="QPushButton" name="expDNdefault">
+              <property name="text">
+               <string>Default</string>
+              </property>
+             </widget>
+            </item>
+            <item>
+             <spacer>
+              <property name="orientation">
+               <enum>Qt::Vertical</enum>
+              </property>
+              <property name="sizeHint" stdset="0">
+               <size>
+                <width>20</width>
+                <height>18</height>
+               </size>
+              </property>
+             </spacer>
+            </item>
+           </layout>
+          </item>
+         </layout>
+        </widget>
+       </item>
+      </layout>
+     </widget>
+     <widget class="QWidget" name="tab_3">
+      <attribute name="title">
+       <string notr="true">PKCS#11 provider</string>
+      </attribute>
+      <layout class="QHBoxLayout" name="horizontalLayout_2">
+       <item>
+        <widget class="QListWidget" name="pkcs11List"/>
+       </item>
+       <item>
+        <layout class="QVBoxLayout" name="verticalLayout">
+         <item>
+          <widget class="QPushButton" name="addButton">
+           <property name="sizePolicy">
+            <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
+             <horstretch>0</horstretch>
+             <verstretch>0</verstretch>
+            </sizepolicy>
+           </property>
+           <property name="text">
+            <string>Add</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <widget class="QPushButton" name="removeButton">
+           <property name="sizePolicy">
+            <sizepolicy hsizetype="Minimum" vsizetype="Fixed">
+             <horstretch>0</horstretch>
+             <verstretch>0</verstretch>
+            </sizepolicy>
+           </property>
+           <property name="text">
+            <string>Remove</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <widget class="QPushButton" name="searchPkcs11">
+           <property name="text">
+            <string>Search</string>
+           </property>
+          </widget>
+         </item>
+         <item>
+          <spacer name="verticalSpacer">
+           <property name="orientation">
+            <enum>Qt::Vertical</enum>
+           </property>
+           <property name="sizeHint" stdset="0">
+            <size>
+             <width>77</width>
+             <height>17</height>
+            </size>
+           </property>
+          </spacer>
+         </item>
+        </layout>
+       </item>
+      </layout>
+     </widget>
     </widget>
    </item>
    <item>

commit 2a0f96e8108566e8ee3b6f7155da6a5d9b5d5a59

Close #39: Subject entries shuffled
Strictly keep order of DistinguishedName entires.

This fixes a bug introduced by dcd940b6b89900c9365e1d88055b4adf01f3f80c
 "SF Feat. Req. #57 More options for Distinguished Name"

widgets/NewX509.cpp
@@ -827,11 +827,12 @@ void NewX509::setX509name(const x509name &n)
 	foreach(nameEdit ne, nameEdits) {
 		ne.edit->setText("");
 	}
-	for (int i=0; i< n.entryCount(); i++) {
+	for (int i=0, j=0; i< n.entryCount(); i++) {
 		int nid = n.nid(i);
 		bool done = false;
 		QStringList sl = n.entryList(i);
-		foreach(nameEdit ne, nameEdits) {
+		for ( ; j < nameEdits.size(); j++) {
+			nameEdit ne(nameEdits[j]);
 			if (nid == ne.nid && ne.edit->text().isEmpty()) {
 				ne.edit->setText(sl[2]);
 				done = true;