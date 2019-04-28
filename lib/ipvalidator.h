58669685eb2eb0c3cec7430df60acf23e4576c2b
Christian Hohnstaedt <christian@hohnstaedt.de>
2019-04-28 08:40:57 +0200

    Close #114: SAN - IPv6 address input not working
    Do not try to write an own IP v4/6 validator.
    The libc function inet_pton() does what we need.

--- lib/ipvalidator.h
+++ lib/ipvalidator.h
@@ -1,6 +1,6 @@
 /* vi: set sw=4 ts=4:
  *
- * Copyright (C) 2005 - 2014 Christian Hohnstaedt.
+ * Copyright (C) 2018 Christian Hohnstaedt.
  *
  * All rights reserved.
  */
@@ -13,40 +13,37 @@
 #include <QString>
 #include <QValidator>
 
+/* Validating IPv4/6 is not as trivial as thought.
+ * - The QHostAddress class requires the network library
+ *   and i don't want to add many megabytes for this validator
+ * - I failed to and actually don't want to write my own validator
+ *   as there are already thoroughly tested functions.
+ * - inet_pton() does what I want, but
+ *   on Windows there is no inet_pton().
+ * - Openssl validates the result anyway.
+ *   So we reduce the validation on windows to the RegEx
+ */
+#if !defined(Q_OS_WIN32)
+#include <arpa/inet.h>
+unsigned char buf[sizeof(struct in6_addr)];
+#else
+#define inet_pton(a,b,c) (1)
+#endif
+
 class ipValidator : public QValidator
 {
     public:
 	QValidator::State validate(QString &input, int&) const
 	{
-		QValidator::State state = Invalid;
-		if (QRegExp("[0-9\\.]*").exactMatch(input)) {
-			// IPv4
-			QStringList octets = input.split(".");
-			bool ok = octets.size() == 4;
-			for (int i=0; ok && i<4; i++) {
-				if (octets[i].toUInt(&ok) > 255)
-					ok = false;
-			}
-			state = ok ? Acceptable : Intermediate;
-		} else if (QRegExp("[0-9a-fA-F:]*").exactMatch(input)) {
-			// IPv6
-			QStringList words = input.split(":");
-			bool ok = true;
-			int empty = 0;
-
-			for (int i=0; empty < 2 && ok && i<words.size(); i++) {
-				empty += words[i].isEmpty() ? 1 : 0;
-				if (words[i].toUInt(&ok, 16) > 0xffff)
-					ok = false;
-			}
-			state = ok ? Acceptable : Intermediate;
-		}
-		qDebug() << "IP Valid:" << input << state;
-		return state;
+		if (!QRegExp("[0-9a-fA-F:\\.]*").exactMatch(input))
+			return Invalid;
+		return inet_pton(AF_INET, CCHAR(input), buf) == 1 ||
+			inet_pton(AF_INET6, CCHAR(input), buf) == 1 ?
+				Acceptable : Intermediate;
 	}
 	void fixup(QString &input) const
 	{
-		QString r = input.toLower();
+		input = input.toLower();
 	}
 };
 #endif
