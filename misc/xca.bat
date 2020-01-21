0152633854f27f5d09b22b34b9601182568eb820
Christian Hohnstaedt <christian@hohnstaedt.de>
2020-01-21 21:56:03 +0100

    Allow double-click xca.bat and examine its output
    Duplicating <ComponentGroupRef Id="GroupBasic" />
    into all features avoids error ICE69,
    but it results in an installation error.
    
    Rather live with the error.

--- misc/xca.bat
+++ misc/xca.bat
@@ -6,4 +6,10 @@ candle.exe -arch x64 -out xca.wixobj xca.wxs
 rem Link: -sw1076: Silence warning LGHT1076
 rem         ICE69: Mismatched component reference.
 
-light.exe -b .. -sw1076 -ext WixUIExtension -ext WixUtilExtension xca.wixobj
+light.exe -b .. -sice:69 -sw1076 -ext WixUIExtension -ext WixUtilExtension xca.wixobj
+
+rem  error LGHT0204 : ICE69 is OK
+
+dir xca.msi
+
+pause
