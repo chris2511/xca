c3d7af01fd209be88d17ef309f45714538db5402
Guido Falsi <mad@madpilot.net>
2020-02-01 18:39:59 +0100

    Add includes required in FreeBSD to use in6_addr, AF_INET and AF_INET6.

--- lib/ipvalidator.h
+++ lib/ipvalidator.h
@@ -13,6 +13,11 @@
 #include <QString>
 #include <QValidator>
 
+#ifdef __FreeBSD__
+#include <sys/socket.h>
+#include <netinet/in.h>
+#endif
+
 /* Validating IPv4/6 is not as trivial as thought.
  * - The QHostAddress class requires the network library
  *   and i don't want to add many megabytes for this validator
