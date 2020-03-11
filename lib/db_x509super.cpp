chris2511/xca

commit b98224599569582dd43242241f8dc9a1f7292a70

Close #163: Show key type/size on column of Certificates tab
Reuse HD_key_type, HD_key_size, HD_key_curve IDs from the key tab.
They use the "hd_key" type to put them into a context-submenu.

The certificate either uses the existing or a temporary key
and calls its "column_data()".

Simplify the submenu logic for the context-menu.

lib/db_x509super.cpp
@@ -77,9 +77,13 @@ dbheaderList db_x509super::getHeaders()
 
 	h <<	new dbheader(HD_x509key_name, false, tr("Key name"),
 			tr("Internal name of the key")) <<
-		new dbheader(HD_x509_sigalg, false, tr("Signature algorithm"));
-
-
+		new dbheader(HD_x509_sigalg, false,
+			tr("Signature algorithm")) <<
+		new key_dbheader(HD_key_type, tr("Key type")) <<
+		new key_dbheader(HD_key_size, tr("Key size")) <<
+#ifndef OPENSSL_NO_EC
+		new key_dbheader(HD_key_curve, tr("EC Group"));
+#endif
 	foreach(int nid, v3nid)
 		h << new nid_dbheader(nid, dbheader::hd_v3ext);
 

lib/headerlist.h
@@ -82,6 +82,7 @@ class dbheader
 		hd_v3ext_ns,
 		hd_number,
 		hd_asn1time,
+		hd_key,
 	};
 	int id;
 	bool show;
@@ -128,6 +129,7 @@ class dbheader
 		switch (id) {
 		case NID_subject_key_identifier:
 		case NID_authority_key_identifier:
+		case HD_key_size:
 			return true;
 		}
 		return type == hd_number;
@@ -227,6 +229,16 @@ class date_dbheader : public dbheader
 		: dbheader(aid, ashow, aname, atip)
 	{
 		type = hd_asn1time;
+	}
+};
+
+class key_dbheader : public dbheader
+{
+    public:
+	key_dbheader(int aid, QString aname)
+		: dbheader(aid, false, aname)
+	{
+		type = hd_key;
 	}
 };
 

lib/pki_x509super.cpp
@@ -157,6 +157,17 @@ QVariant pki_x509super::column_data(const dbheader *hd) const
 	if (hd->id == HD_x509_sigalg) {
 		return QVariant(getSigAlg());
 	}
+
+	if (hd->type == dbheader::hd_key) {
+		QVariant v;
+		pki_key *key = getRefKey(), *tmpkey = NULL;
+		if (!key)
+			tmpkey = key = getPubKey();
+		if (key)
+			v = key->column_data(hd);
+		delete tmpkey;
+		return v;
+	}
 	if (hd->type == dbheader::hd_v3ext ||
 	    hd->type == dbheader::hd_v3ext_ns)
 	{

widgets/XcaTreeView.cpp
@@ -306,11 +306,19 @@ void XcaTreeView::columnRemove(void)
 		curr_hd->action->setChecked(false);
 }
 
+static void addSubmenu(tipMenu *menu, tipMenu *sub)
+{
+	if (sub->isEmpty())
+		delete sub;
+	else
+		menu->addMenu(sub);
+}
+
 void XcaTreeView::contextMenu(QContextMenuEvent *e, QMenu *parent, int col)
 {
 	int shown = 0;
-	tipMenu *menu, *dn, *v3ext, *current, *v3ns;
-	QAction *a, *sep;
+	tipMenu *menu, *dn, *v3ext, *current, *v3ns, *keyprop;
+	QAction *a;
 	dbheader *hd;
 	dbheaderList allHeaders = basemodel->getAllHeaders();
 
@@ -318,6 +326,7 @@ void XcaTreeView::contextMenu(QContextMenuEvent *e, QMenu *parent, int col)
 	dn = new tipMenu(tr("Subject entries"), mainwin);
 	v3ext = new tipMenu(tr("X509v3 Extensions"), mainwin);
 	v3ns = new tipMenu(tr("Netscape extensions"), mainwin);
+	keyprop = new tipMenu(tr("Key properties"), mainwin);
 	menu->addAction(tr("Reset"), basemodel, SLOT(columnResetDefaults()));
 	if (col >= 0 && col < allHeaders.size()) {
 		curr_hd = allHeaders[col];
@@ -328,7 +337,7 @@ void XcaTreeView::contextMenu(QContextMenuEvent *e, QMenu *parent, int col)
 	} else {
 		curr_hd = NULL;
 	}
-	sep = menu->addSeparator();
+	menu->addSeparator();
 	foreach(hd, allHeaders) {
 		switch (hd->type) {
 			case dbheader::hd_x509name:
@@ -342,6 +351,9 @@ void XcaTreeView::contextMenu(QContextMenuEvent *e, QMenu *parent, int col)
 					continue;
 				current = v3ns;
 				break;
+			case dbheader::hd_key:
+				current = keyprop;
+				break;
 			default:
 				current = menu;
 				break;
@@ -352,26 +364,15 @@ void XcaTreeView::contextMenu(QContextMenuEvent *e, QMenu *parent, int col)
 		a->setToolTip(hd->getTooltip());
 		hd->action = a;
 	}
-	if (!dn->isEmpty() || !v3ext->isEmpty())
-		menu->insertSeparator(sep);
 
-	if (!dn->isEmpty())
-		menu->insertMenu(sep, dn);
-	else
-		delete dn;
+	menu->addSeparator();
 
-	if (!v3ext->isEmpty()) {
-		if (!v3ns->isEmpty()) {
-			v3ext->addSeparator();
-			v3ext->addMenu(v3ns);
-		} else {
-			delete v3ns;
-		}
-		menu->insertMenu(sep, v3ext);
-	} else {
-		delete v3ext;
-		delete v3ns;
-	}
+	addSubmenu(menu, dn);
+	addSubmenu(v3ext, v3ns);
+	addSubmenu(menu, v3ext);
+	addSubmenu(menu, keyprop);
+
+
 	if (parent) {
 		parent->addSeparator();
 		parent->addMenu(menu)->setText(tr("Columns"));

commit a1fedfaf9eb18d0f4c88dbf7081719fdd342dc02

Fix building against OpenSSL without EC.
This commit amends b98224599569582dd43242241f8dc9a1f7292a70
which broke the NO_EC build

lib/db_x509super.cpp
@@ -80,10 +80,11 @@ dbheaderList db_x509super::getHeaders()
 		new dbheader(HD_x509_sigalg, false,
 			tr("Signature algorithm")) <<
 		new key_dbheader(HD_key_type, tr("Key type")) <<
-		new key_dbheader(HD_key_size, tr("Key size")) <<
+		new key_dbheader(HD_key_size, tr("Key size"))
 #ifndef OPENSSL_NO_EC
-		new key_dbheader(HD_key_curve, tr("EC Group"));
+	  <<	new key_dbheader(HD_key_curve, tr("EC Group"))
 #endif
+		;
 	foreach(int nid, v3nid)
 		h << new nid_dbheader(nid, dbheader::hd_v3ext);
 