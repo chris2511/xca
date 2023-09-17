5e346353e63cbe826bd0890c90c0f25f8db8e4a4
Christian Hohnstaedt <christian@hohnstaedt.de>
2023-09-17 08:37:17 +0200

    Document the attempt to build universal mysql plugin
    It only builds "x86_64":
    $ file build-sqlplugins/plugins/sqldrivers/libqsqlmysql.dylib
    build-sqlplugins/plugins/sqldrivers/libqsqlmysql.dylib: Mach-O 64-bit bundle x86_64
    
    Building with -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" has no effect.
    Neither has './configure -- -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64"'
    Even './configure -- -DCMAKE_OSX_ARCHITECTURES="arm64"' only builds
    x86_64 plugins.

--- release/build-mac.sh
+++ release/build-mac.sh
@@ -3,6 +3,8 @@ set -x
 
 do_openssl()
 {
+  test -x $INSTALL_DIR/lib/libcrypto.dylib && return
+
   test -f "$OSSL".tar.gz || curl -O https://www.openssl.org/source/"$OSSL".tar.gz
   test -d "$OSSL" || tar zxf "$OSSL".tar.gz
   mkdir -p ${INSTALL_DIR}/lib
@@ -22,10 +24,48 @@ do_openssl()
   lipo -create -output "$INSTALL_DIR"/lib/libssl.${OSSL_MAJOR}.dylib $PARTS_ssl
 }
 
+# need to install ninja via "brew install ninja"
+do_mysql()
+{
+  PLUGIN="$QT_DIR/plugins/sqldrivers/libqsqlmysql.dylib"
+  #test -f "$PLUGIN" && return
+
+  B="$TOP_DIR/mariadb-build"
+  mkdir -p ${B}
+  cmake -B ${B} -DCMAKE_C_FLAGS=-Wno-deprecated-non-prototype \
+	   -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+	   -DCMAKE_PREFIX_PATH="$INSTALL_DIR" \
+	   -DINSTALL_LIBDIR=lib \
+	   $TOP_DIR/mariadb-connector-c-3.3.7-src
+  cmake --build ${B} -j$JOBS
+  cmake --install ${B} --prefix="$INSTALL_DIR"
+
+#############################
+
+  SQL_BUILD="$TOP_DIR/build-sqlplugins"
+  mkdir -p "$SQL_BUILD"
+
+  cmake -B $SQL_BUILD \
+	-DCMAKE_INSTALL_PREFIX="$QT_DIR" \
+	-DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+	-DMySQL_INCLUDE_DIR="$INSTALL_DIR/include/mariadb" \
+	-DMySQL_LIBRARY="$INSTALL_DIR/lib/libmariadb.dylib" \
+	-DFEATURE_sql_odbc=OFF -DFEATURE_sql_sqlite=OFF \
+	"$QT_DIR/../Src/qtbase/src/plugins/sqldrivers"
+
+  cmake --build $SQL_BUILD -j$JOBS
+  cmake --install $SQL_BUILD
+  install_name_tool -change libmariadb.3.dylib "$INSTALL_DIR"/lib/libmariadb.3.dylib \
+                              $QT_DIR/plugins/sqldrivers/libqsqlmysql.dylib
+  file  $QT_DIR/plugins/sqldrivers/libqsqlmysql.dylib
+  file  $SQL_BUILD/plugins/sqldrivers/libqsqlmysql.dylib
+}
+
 OSSL_MAJOR="3"
 OSSL="openssl-3.0.10"
 XCA_DIR="$(cd `dirname $0`/.. && pwd)"
 TOP_DIR="`dirname $XCA_DIR`"
+QT_DIR="$TOP_DIR/6.6.0/macos"
 
 BUILDDIR="$TOP_DIR/osx-release"
 
@@ -33,11 +73,15 @@ INSTALL_DIR="$TOP_DIR/install"
 SDK="11"
 JOBS=7
 
-test -x $INSTALL_DIR/lib/libcrypto.dylib || (cd $TOP_DIR && do_openssl )
+cd $TOP_DIR
+
+do_openssl
+# Only builds x86_64 binaries, hell knows why...
+# do_mysql
 
 cmake -B "$BUILDDIR" "$XCA_DIR" \
 	-DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
-	-DCMAKE_PREFIX_PATH="$TOP_DIR/6.6.0/macos/lib/cmake;$INSTALL_DIR" \
+	-DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake;$INSTALL_DIR" \
 	-DCMAKE_OSX_DEPLOYMENT_TARGET=$SDK
 
 cmake --build "$BUILDDIR" -j$JOBS
